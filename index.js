ef619b9f56b388d8d25061f031be906f55a898a8
unknown <ndiweninqaba@gmail.com>
2022-10-11 12:22:49 +0200

    cronjob to delete old appointments

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const http = require('http');
+const cron = require('node-cron');
 const  typeDefs = require('./typeDefs');
 const resolvers = require('./resolvers');
 const mongoose = require('mongoose');
 const loc = 'mongodb://127.0.0.1:27017/cohortsdb'
 const host = '0.0.0.0';
+const Appointment = require('./models/appointment.model')
 
 const createContext = (request) => {
 
@@ -35,7 +37,7 @@ async function startApolloServer() {
     
     server.applyMiddleware({ app });
     await new Promise(resolve => httpServer.listen(process.env.PORT || 3000, resolve));
-    await mongoose.connect(process.env.MONGODB_URI,{
+    await mongoose.connect(loc,{
             useUnifiedTopology: true,
             useNewUrlParser: true
         })
@@ -48,3 +50,35 @@ async function startApolloServer() {
   
 }
 startApolloServer()
+
+cron.schedule('1 1 1 * *', async () => {
+var todayDate = new Date()
+var day = todayDate.getDate()
+var month = todayDate.getMonth() + 1
+var year = todayDate.getFullYear()
+var formated = `${day}-${month}-${year}`
+console.log("cron Job Deleting Old Appointments")
+var visits=[]
+try {
+  const appointments = await Appointment.find({})
+  const convertToDate = (d) => {
+    const [day, month, year] = d.split("-");
+    return new Date(year, month - 1, day);
+  }
+ 
+ for(var i=0;i<appointments.length;i++){
+    if(convertToDate(appointments[i].visit)<convertToDate(formated)){
+      visits.push(appointments[i].visit)
+    }
+ }
+
+ await Appointment.deleteMany({ visit: { $in: visits } })
+
+  
+} catch (error) {
+  console.log(error)
+}
+ 
+ 
+
+});
